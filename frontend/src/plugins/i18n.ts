0462c8d2d66f8ab1689934bff6c45fb569801336
rubenschulze <ruben.schulze@adesso.de>
2025-08-18 16:13:54 +0200

    fixed i18n issues

--- frontend/src/plugins/i18n.ts
+++ frontend/src/plugins/i18n.ts
@@ -1,12 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
-console.log('i18n messages:', messages)
 
 const i18n = createI18n({
+  legacy: false,
   locale: 'de',
   fallbackLocale: 'en',
-  messages: messages,
+  messages
 })
 
 export default i18n
