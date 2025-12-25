20c5245185fd1cccb0cd4963fd08e55f79d98efa
rubenschulze <ruben.schulze@adesso.de>
2025-12-26 00:01:43 +0100

    feat: added a public bucket to the infra and cicd

--- deployments/dev/0-infra/storage.tf
+++ deployments/dev/0-infra/storage.tf
@@ -11,3 +11,33 @@ resource "google_storage_bucket" "exported_pdfs" {
 
   depends_on = [google_project_service.apis]
 }
+
+# Storage bucket for public images
+resource "google_storage_bucket" "public_images" {
+  name          = "${var.project_id}-swim-gen-public-images"
+  location      = var.region
+  project       = var.project_id
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  uniform_bucket_level_access = true
+
+  cors {
+    origin          = ["*"]
+    method          = ["GET", "HEAD", "OPTIONS"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+
+  depends_on = [google_project_service.apis]
+}
+
+# Make the bucket public
+resource "google_storage_bucket_iam_member" "public_images_iam" {
+  bucket = google_storage_bucket.public_images.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
